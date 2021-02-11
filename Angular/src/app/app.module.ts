c8325f226a2edc90ef6e6252a7f5621a7d2e7281
Alexey Khludov <alexey.khludov@devexpress.com>
2021-02-11 20:30:16 +0300

    bump vue package-lock, modify imports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxPivotGridModule, DxDataGridModule, DxPopupModule, DxTemplateModule } from 'devextreme-angular';
+import { DxPivotGridModule } from 'devextreme-angular/ui/pivot-grid';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxPopupModule } from 'devextreme-angular/ui/popup';
+import { DxTemplateModule } from 'devextreme-angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
